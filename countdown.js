6c08e827b3170340f44c861d92e0336273686b4e
iidiong <idong30@hotmail.com>
2021-05-25 08:28:24 -0400

    .

--- countdown.js
+++ countdown.js
@@ -1,8 +1,13 @@
 /*
-Description:
+Exercise 1:
 Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log “DONE!” and stop.
+
+Description:
+The countdown timer accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0, it displays “DONE!” and stop.
+
 Input: 
     Any positive number (n)
+
 Output: 
     - Display (n -1) every 1000 milliseconds
     - Display done when n = 0
--- randomGame.js
+++ randomGame.js
@@ -1,7 +1,11 @@
 /* 
-Description:
+Exercise 2:
 Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter.If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75. 
+
+Description:
+RandomGame selects a random number between 0 and 1 every 1000 milliseconds and each time a random number is picked, a counter is increamented.If the picked number is greater than .75, the timer is stopped and the number of tries it took before the number greater than .75 is picked.
 */
+
 let intervalId = 0;
 let counter = 0;
 
