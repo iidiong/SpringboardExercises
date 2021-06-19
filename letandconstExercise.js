753a9bd695ada66f7ee18572f7e869966d95ec14
iidiong <idong30@hotmail.com>
2021-06-18 21:01:55 -0400

    Completed let and const exercises

---
+++ letandconstExercise.js
@@ -0,0 +1,24 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+// ES2015 Global Constants
+
+const PI = 3.14;
+PI = 42;
+
+
+/*
+What is the difference between var and let?
+    Variabble declared with var is global or local to the function in which it is defined
+    Variable declared with let is limited in scope to the block, statement, or expression being used
+    Variable declared with var can be redeclared while variable declared with let cannot be redeclare
+What is the difference between var and const?
+    Variable declared with const maintains constant value while variable declared with var does not
+What is the difference between let and const?
+    Variable declared with let can be reassigned while variable declared with const cannnot
+What is hoisting?
+    Hoisting is a condtion inwhich the variable or functions decleared are moved to the top of scope before execution
+
+*/
+
